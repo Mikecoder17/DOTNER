b168966a2a1563f92cd08dd708eb053a37b00565
mikecoder17 <mikwiatkowski111100@gmail.com>
2025-07-06 21:35:34 +0200

    updated init

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+399111dd54b9a45e1fa71d03b389575c7478ffa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e39e89a2d959cb81398b6b29e26869cbc7d5408")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
